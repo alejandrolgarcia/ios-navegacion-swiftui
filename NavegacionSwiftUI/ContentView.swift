ba483195c6e4104be424c63e48642ea3341ddd52
Luis Garcia <lalejandro.1988@gmail.com>
2021-12-31 12:00:38 -0800

    Initial Commit

---
+++ NavegacionSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavegacionSwiftUI
+//
+//  Created by Luis Garcia on 31/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavegacionSwiftUI/NavegacionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavegacionSwiftUIApp.swift
+//  NavegacionSwiftUI
+//
+//  Created by Luis Garcia on 31/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NavegacionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
